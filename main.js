375819d42723058ebfccaff7dc80d663e5ff217d
Soumya123-l <82278150+Soumya123-l@users.noreply.github.com>
2022-01-15 09:19:53 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
-var SpeechRecongnition = window.webkitSpeechRecogntition;
-
-var recognition = new SpeechRecogntition();
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
 
 function start()
 {
-    document.getElementById("textbox").innerHTML = "";
+    document.getElementById("textbox").innerHTML = ""; 
     recognition.start();
-}
+} 
+ 
+recognition.onresult = function(event) {
 
-recogntition.onresult = function run (event) {
-console.log(event);
+     console.log(event); 
 
-    var Content = event.results[0] [0].transript;
+    var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-
-    document.getElementById("tectbox").innerHTML = Content;
+        speak();
 }
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").innerHTML;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
